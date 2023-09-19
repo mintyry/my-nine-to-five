7b8251b037d7f3a2176ad11bc8fd7fcc8d3cd174
mintyry <gitmintyry@gmail.com>
2023-09-19 16:16:58 -0700

    went through and sorted comments out and deleted instructional comments

--- assets/script.js
+++ assets/script.js
@@ -1,73 +1,41 @@
-/* GIVEN I am using a daily planner to create a schedule
-
-
-WHEN I view the time blocks for that day
-THEN each time block is color-coded to indicate whether it is in the past, present, or future
-
- */
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. 
-
 /* $ function () {}; is a shorthand syntax for the $(document).ready() function., 
 which ensures code isn't run until bwoser has finished rendering all HTMl. */
-
-
 $(function () {
+    // Note: I keep console.logs in here but commented out to demonstrate any testing that proved to be helpful
+    // This function checks what time it is and then compares that time to the div's hour (which we parsed out of the id string)
+    // The if statement in the function will then dynamically add classes to apply corresponding attributes.
+    function checkTime() {
+        let currentHour = dayjs().format('H');
+        // console.log(typeof currentHour); // string!
+        let numberCurrentHour = parseInt(currentHour);
+        // console.log(numberCurrentHour)
+        // console.log(typeof numberCurrentHour)
+        let timeDiv = $('.time-block');
+        // console.log(timeDiv); // yields all time divs
+        // console.log(timeDiv[1]); // yields hour-10 div
 
-   
+        timeDiv.each(function () {
+            // We access each timeDiv's id attribute (eg: hour-9, etc.), then we split it (hour, 9), access index 1 (9),
+            //then parse it to make it a number
+            let hourId = parseInt($(this).attr('id').split('-')[1]);
+            // console.log(hourId) // yields every div's hour number
 
-
-    // maybe if statement goes in for loop
-    // if (i === currentHour) {
-    //     `#hour-${i}`.addClass('present')
-    // }
-
-    // else if (time < now) {
-    //     'hour-${i}`.addClass('past')
-    // } else {
-    //     'hour-${i}`.addClass('future')
-    // }
-
-    /* can we put `hour-${i}` in a variable? I tried but code wouldn't display properly
-    */
-
-
-function checkTime () {
-let currentHour = dayjs().format('H');
-    console.log(currentHour);
-
-   let timeDiv = $('.time-block');
-    console.log(timeDiv[1]);
-
-   console.log(timeDiv); // yields all time divs
-//    console.log(hourId); //yields hour-9
-
-   timeDiv.each(function () {
-    // let hourId = $(this).attr('id');
-    let hourId = parseInt($(this).attr('id').split('-')[1]);
-    // console.log(this)
-    console.log(hourId)
-
-    if (hourId === currentHour) {
-        $(this).addClass('present');
+            // The if statement will dynamically add classes to apply corresponding attributes.
+            if (hourId === numberCurrentHour) {
+                $(this).addClass('present');
+            } else if (hourId < numberCurrentHour) {
+                $(this).addClass('past');
+            } else {
+                $(this).addClass('future');
+            }
+            // console.log(this); //yields every div class of "time-block"
+        });
     }
-
-    console.log(this);
-   });
-
-  
-}
-
-checkTime();
+    //actually execute the function
+    checkTime();
 
 
-
+    // This function will show the current date and time in the header, formatted in my preference
     function showDate() {
         let dayEl = $('#currentDay');
         let day = dayjs();
@@ -77,7 +45,8 @@ checkTime();
     };
 
     showDate();
-    setInterval(showDate, 200);
+    //will display a running clock
+    setInterval(showDate, 500);
 
     /* For loop will run through hours 9-17, looping each i for the subsequent id/hour.
      We access elements by traversing DOM rather than hardcoding for each hour; we do this by accessing saveBtn class, which they all share,
@@ -88,10 +57,7 @@ checkTime();
         $(`#hour-${i}`).children('textarea').text(localStorage.getItem(`hour-${i}`));
     }
 
-
-
     let save = $('.saveBtn');
-
     save.on('click', function () {
         let userText = $(this).siblings('textarea').val();
         let scheduleId = $(this).parent().attr('id');
@@ -106,20 +72,4 @@ checkTime();
         // console.log($(this).parent());
         // console.log($(this).parent().attr('id'));
     });
-
-    /* HINT: What does `this` reference in the click listener function? 
-    How can DOM traversal be used to get the "hour-x" id of the
-    time-block containing the button that was clicked? 
-    How might the id be useful when saving the description in local storage? */
-    //
-    // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. 
-    /* HINTS: How can the id attribute of each time-block be used to conditionally add or remove
-     the past, present, and future classes? 
-     How can Day.js be used to get the current hour in 24-hour time? */
-    //
-    // TODO: Add code to get any user input that was saved in localStorage 
-    // and set the values of the corresponding textarea elements. 
-    /* HINT: How can the id attribute of each time-block be used to do this? */
-    //
-    // TODO: Add code to display the current date in the header of the page.
 });
